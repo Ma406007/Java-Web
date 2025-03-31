05d601f04f41bd353367b202921cb620493870ab
cai-yuanxiang-ma406007 <g2047019077@qq.com>
2025-03-31 21:49:02 +0800

    Java-Web PracticeDay 10

---
+++ PracticeDay10/Tias-web-management/src/main/java/org/example/mapper/EmpExprMapper.java
@@ -0,0 +1,13 @@
+package org.example.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.example.pojo.EmpExpr;
+
+import java.util.List;
+
+//员工工作经历
+@Mapper
+public interface EmpExprMapper {
+    //批量插入员工工作经历信息
+    void insertBatch(List<EmpExpr> exprList);
+}
