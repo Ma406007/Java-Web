b4e65774fbca4c3d5864688c49bbe2dac65de0fa
Ma406007 <g2047019077@qq.com>
2025-04-18 15:54:24 +0800

    Java-Web PracticeDay 12(repair the bug of Day 11)
    Java-Web PracticeDay 12(repair the bug of Practice Day 11)

---
+++ PracticeDay12/Tlias-Web-Management/src/main/java/org/example/service/Impl/ReportServiceImpl.java
@@ -0,0 +1,32 @@
+package org.example.service.Impl;
+
+import org.example.mapper.EmpMapper;
+import org.example.pojo.JobOption;
+import org.example.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ReportServiceImpl implements ReportService {
+
+    @Autowired
+    private EmpMapper empMapper;
+
+    @Override
+    public JobOption getEmpJobData() {
+        List<Map<String, Object>> list = empMapper.countEmpJobData();
+
+        List<Object> jobList = list.stream().map(dataMap -> dataMap.get("pos")).toList();
+
+        List<Object> dataList = list.stream().map(dataMap -> dataMap.get("num")).toList();
+        return new JobOption(jobList, dataList);
+    }
+
+    @Override
+    public List<Map> getEmpGenderData() {
+        return empMapper.countEmpGenderData();
+    }
+}
